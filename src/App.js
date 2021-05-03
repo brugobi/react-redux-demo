2af9ba3cfde6099a96814212306c524d910f33df
brugobi <brgobbi@hotmail.com>
2021-05-03 13:47:05 -0400

    add hooks to do the same as connect does

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+        {/* <CakeContainer /> */}
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
